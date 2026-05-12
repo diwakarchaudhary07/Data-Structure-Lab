79b01ad3636ad16c678e03a73cabb2fba3e4a2d3
diwakar chaudhary <boyroyal4853@gmail.com>
2026-05-12 23:56:47 +0530

    add

---
+++ Experiment29.c
@@ -0,0 +1,22 @@
+// Program 29: GCD Without Recursion
+// Program
+#include <stdio.h>
+
+int main()
+{
+    int a, b, temp;
+
+    printf("Enter two numbers: ");
+    scanf("%d%d", &a, &b);
+
+    while(b != 0)
+    {
+        temp = b;
+        b = a % b;
+        a = temp;
+    }
+
+    printf("GCD = %d", a);
+
+    return 0;
+}
